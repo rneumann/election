b3c2ed2abc349b0faa3671de7dbe713607d3cc13
grso1012 <grimpe.soulaiman@gmail.com>
2025-10-31 19:47:38 +0100

    tailwind config angepasst

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       colors: {
-        'hka-red': '#E2001A',
-        'hka-dark': '#333333',
-        'hka-gray': '#666666',
-        'hka-light-gray': '#F5F5F5',
+        "hka-red": "#E2001A",
+        "hka-dark": "#333333",
+        "hka-gray": "#666666",
+        "hka-light-gray": "#F5F5F5",
       },
     },
   },
