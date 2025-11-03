819af147a10fc37001777f8c991ab0db5f699c0a
Raphael <theissraphael82@gmail.com>
2025-11-03 23:28:59 +0100

    added dockerfile, dockerignore, compose

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -1,28 +1,57 @@
-import { v4 as uuidv4 } from 'uuid';
+import { promises as fs, constants } from 'fs';
 import { Client } from 'ldapts';
 import 'dotenv/config';
 import { logger } from '../conf/logger/logger.js';
 
 /**
+ * Reads a secret from a file at /run/secrets/${name}.
+ * If the file does not exist, it falls back to the environment variable
+ * with the same name. If this variable is also not set, it returns the
+ * optional fallback value. If no fallback is provided, it throws an
+ * error.
+ *
+ * @param {string} name Name of the secret to read.
+ * is not found.
+ * @returns {Promise<string>} The secret value.
+ * @throws {Error} If the secret is not found and no fallback is provided.
+ */
+const readSecret = async (name) => {
+  const path = `/run/secrets/${name}`;
+  try {
+    // Prüft, ob Datei existiert
+    await fs.access(path, constants.F_OK);
+    const data = await fs.readFile(path, 'utf8');
+    return data.trim();
+  } catch {
+    // eslint-disable-next-line
+    const envValue = process.env[name];
+    if (envValue) {
+      return envValue;
+    }
+    throw new Error(`Secret for ${name} not found`);
+  }
+};
+
+/**
  * Returns an object with the admin user's username and password.
  * The password is set to the value of the ADMIN_PASSWORD environment variable,
  * or a randomly generated UUID v4 if this variable is not set.
- * @returns {object} an object with the admin user's username and password
+ * @returns {Promise<object>} an object with the admin user's username and password
  */
-const getAdminUser = () => ({
+const getAdminUser = async () => ({
   username: 'admin',
-  password: process.env.ADMIN_PASSWORD || uuidv4(),
+  password: await readSecret('ADMIN_PASSWORD'),
 });
 
 /**
  * Returns an object with the committee user's username and password.
  * The password is set to the value of the COMMITTEE_PASSWORD environment variable,
  * or a randomly generated UUID v4 if this variable is not set.
- * @returns {object} an object with the committee user's username and password
+ * @returns {Promise<object>} an object with the committee user's username and password
  */
-const getCommitteeUser = () => ({
+const getCommitteeUser = async () => ({
   username: 'committee',
-  password: process.env.COMMITTEE_PASSWORD || uuidv4(),
+  password: await readSecret('COMMITTEE_PASSWORD'),
 });
 
 /**
@@ -34,17 +63,18 @@ const getCommitteeUser = () => ({
  */
 export const login = async (username, password) => {
   const { AD_URL, AD_BASE_DN, AD_DOMAIN } = process.env;
+  const adminUser = await getAdminUser();
+  const committeeUser = await getCommitteeUser();
 
   username = username.trim();
-  password = password.trim();
   // Check for special admin user
-  if (username === getAdminUser().username && password === getAdminUser().password) {
+  if (username === adminUser.username && password === adminUser.password) {
     logger.debug('Admin user authenticated successfully.');
-    return { username: getAdminUser().username, role: 'admin' };
+    return { username: adminUser.username, role: 'admin' };
   }
-  if (username === getCommitteeUser().username && password === getCommitteeUser().password) {
+  if (username === committeeUser.username && password === committeeUser.password) {
     logger.debug('Committee user authenticated successfully.');
-    return { username: getCommitteeUser().username, role: 'committee' };
+    return { username: committeeUser.username, role: 'committee' };
   }
   // Normal user via LDAP
   if (!AD_URL || !AD_BASE_DN || !AD_DOMAIN) {
