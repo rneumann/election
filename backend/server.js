8529c15945f16717196c6a732a5f7edbff6de0cf
Raphael <theissraphael82@gmail.com>
2025-11-07 23:35:09 +0100

    added Banner

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import os from 'os';
 import dotenv from 'dotenv';
 import { logger } from './src/conf/logger/logger.js';
 import { app } from './src/app.js';
@@ -5,5 +6,19 @@ dotenv.config();
 
 const { PORT, NODE_ENV } = process.env;
 app.listen(PORT, () => {
-  logger.info(`Server running in ${NODE_ENV} mode on http://localhost:${PORT}`);
+  logger.info(String.raw`
+
+'    ######## ##       ########  ######  ######## ####  #######  ##    ##    
+'    ##       ##       ##       ##    ##    ##     ##  ##     ## ###   ##    
+'    ##       ##       ##       ##          ##     ##  ##     ## ####  ##    
+'    ######   ##       ######   ##          ##     ##  ##     ## ## ## ##    
+'    ##       ##       ##       ##          ##     ##  ##     ## ##  ####    
+'    ##       ##       ##       ##    ##    ##     ##  ##     ## ##   ###    
+'    ######## ######## ########  ######     ##    ####  #######  ##    ##   
+ 
+    `);
+  logger.info(`Server running on port: ${PORT}`);
+  logger.info(`Environment: ${NODE_ENV}`);
+  logger.info(`Hostname: ${os.hostname()}`);
+  logger.info(`Node Verison: ${process.version}`);
 });
