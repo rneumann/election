ad2ece2ebdd56c1ae89f2b66597fc938bbdf421d
Raphael <theissraphael82@gmail.com>
2026-01-09 19:05:02 +0100

    fix database, compose

--- backend/.extras/compose/postgres/initdb/prod/00_create_schema.sql
+++ backend/.extras/compose/postgres/initdb/prod/00_create_schema.sql
@@ -1,15 +1,14 @@
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname='election') THEN
-    CREATE ROLE election LOGIN PASSWORD 'election';
-  END IF;
-END$$;
-
+-- Schema
 CREATE SCHEMA IF NOT EXISTS election AUTHORIZATION election;
 
-ALTER ROLE election SET search_path TO election, public;
+ALTER ROLE election
+SET
+  search_path TO election,
+  public;
 
-CREATE EXTENSION IF NOT EXISTS "pgcrypto" SCHEMA election;
+-- Extensions gehören i.d.R. in public
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 ALTER SCHEMA election OWNER TO election;
+
 GRANT USAGE ON SCHEMA election TO election;
